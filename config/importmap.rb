d98aada44fd480fc9a03adb32c59b9189a4b6426
JUSTIN BAUTISTA <justinbautista0921@gmail.com>
2024-07-17 01:08:37 -0400

    My First commit

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,4 @@
 # Pin npm packages by running ./bin/importmap
-
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
@@ -7,3 +6,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "flatpickr" # @4.6.13
